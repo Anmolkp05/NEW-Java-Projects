08367dbb3b500e78827cfaeba893e4a9050c760c
anmol kp <anmolkamalapuri@gmail.com>
2024-07-27 02:01:33 +0530

    first commit

--- IntegerToRoman.java
+++ IntegerToRoman.java
@@ -1,6 +1,6 @@
 public class IntegerToRoman {
     public static void main(String[] args) {
-        System.out.println(intToRoman(1200));
+        System.out.println(intToRoman(2000));
     }
 
         public static String intToRoman(int num) {
