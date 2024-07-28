d954ced88ced5337666143804cb85e2b34f1144a
anmol kp <anmolkamalapuri@gmail.com>
2024-07-29 01:01:01 +0530

    first commit

--- FirstAndLastPosition.java
+++ FirstAndLastPosition.java
@@ -2,27 +2,32 @@ import java.util.Arrays;
 
 public class FirstAndLastPosition {
     public static void main(String[] args) {
-        int[] nums = {7,7,5,5,5,8};
-        int target =5 ;
+        int[] nums = {7, 7, 5, 5, 5, 8};
+        int target = 5;
         System.out.println(Arrays.toString(searchRange(nums, target)));
     }
 
-    public static int[] searchRange(int[] arr, int target) {
-        int firstPosition = -1;
-        int lastPosition = -1;
+        public static int[] searchRange(int[] nums, int target) {
+            int[] arr = new int[2];
+            arr[0] = arr[1] = -1;
 
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == target) {
-                if (firstPosition == -1) {
-                    firstPosition = i;
+
+            boolean foundFirst = false;
+            for (int i = 0; i < nums.length; i++) {
+                if (nums[i] == target) {
+                    if (!foundFirst) {
+                        arr[0] = i;
+                        foundFirst = true;
+                    }
+                    arr[1] = i;
                 }
-                lastPosition = i;
             }
-        }
 
-        return new int[] { firstPosition, lastPosition };
+            return arr;
+        }
     }
-}
+
+
 
 
 
