bfb91b01a0129a214d968bd3d426c680af58fa75
anmol kp <anmolkamalapuri@gmail.com>
2024-09-06 02:45:35 +0530

    first commit

--- RotateArray.java
+++ RotateArray.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class RotateArray {
     public static void main(String[] args) {
         int  nums[]= {1,2,3};
-        int k = 4;
+        int k = 6;
         System.out.println(Arrays.toString(rotate(nums,k)));
     }
     public static int[] rotate(int[] nums, int k) {
@@ -18,6 +18,7 @@ public class RotateArray {
     }
     public static void reverse (int[] nums , int start, int end )
     {
+
         while (start<=end)
         {
             int temp = nums[start];
