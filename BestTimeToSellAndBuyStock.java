ccb4baebc65fde5661f5fa814913f1f2e5e27618
anmol kp <anmolkamalapuri@gmail.com>
2024-10-03 00:21:34 +0530

    first commit

---
+++ BestTimeToSellAndBuyStock.java
@@ -0,0 +1,19 @@
+
+public class BestTimeToSellAndBuyStock {
+    public static void main(String[] args) {
+        int[] prices = {7, 1, 5, 3, 6, 4};
+        int maxProfit = maxProfit(prices);
+        System.out.println("Maximum Profit: " + maxProfit);
+    }
+        public static int maxProfit(int[] prices) {
+            int maxProfit = 0;
+            for (int i = 1; i < prices.length; i++) {
+                if (prices[i] > prices[i - 1]) {
+                    maxProfit += prices[i] - prices[i - 1];
+                }
+            }
+            return maxProfit;
+        }
+    }
+
+
