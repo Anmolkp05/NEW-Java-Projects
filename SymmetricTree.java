69e71dfadc4f873f2783894aa91a8a5c2d5670e7
anmol kp <anmolkamalapuri@gmail.com>
2024-10-06 11:46:47 +0530

    first commit

--- SymmetricTree.java
+++ SymmetricTree.java
@@ -3,10 +3,10 @@ public class SymmetricTree {
         TreeNode root = new TreeNode(1);
         root.left = new TreeNode(2);
         root.right = new TreeNode(2);
-        root.left.left = new TreeNode(3);
+        root.left.left = new TreeNode(4);
         root.left.right = new TreeNode(4);
         root.right.left = new TreeNode(4);
-        root.right.right = new TreeNode(3);
+        root.right.right = new TreeNode(4);
 
         SymmetricTree st = new SymmetricTree();
         System.out.println("Is the tree symmetric? " + st.isSymmetric(root));
