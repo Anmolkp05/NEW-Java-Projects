96854d4b97382e78754a2dc18f2cae9ad02a1a02
anmol kp <anmolkamalapuri@gmail.com>
2025-01-30 00:57:41 +0530

    first commit

---
+++ MaxDepthOfBinaryTree.java
@@ -0,0 +1,42 @@
+public class MaxDepthOfBinaryTree {
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(9);
+        root.right = new TreeNode(20);
+        root.right.left = new TreeNode(15);
+        root.right.right = new TreeNode(7);
+
+        Solution solution = new Solution();
+        int depth = solution.maxDepth(root);
+
+        System.out.println("Maximum Depth: " + depth);
+    }
+
+    static class Solution {
+        public int maxDepth(TreeNode root) {
+            if (root == null) {
+                return 0;
+            }
+
+            int leftDepth = maxDepth(root.left);
+            int rightDepth = maxDepth(root.right);
+
+            return Math.max(leftDepth, rightDepth) + 1;
+        }
+    }
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
