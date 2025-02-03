66d46e5c42caf65942b1eb9fc3abd64325ce6724
anmol kp <anmolkamalapuri@gmail.com>
2025-02-03 23:59:33 +0530

    first commit

--- GroupAnagrams.java
+++ GroupAnagrams.java
@@ -2,27 +2,31 @@ import java.util.*;
 
 public class GroupAnagrams {
     public static void main(String[] args) {
-        String[] strs1 = {"eat", "tea", "tan", "ate", "nat", "bat"};
-        List<List<String>> result = groupAnagrams(strs1);
-        System.out.println(result);
+        String[] strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
+        List<List<String>> result = groupAnagrams(strs);
 
+        for (List<String> group : result) {
+            System.out.println(group);
 
     }
 
         public static List<List<String>> groupAnagrams(String[] strs) {
-            List<List<String>> result = new ArrayList<>();
             Map<String, List<String>> map = new HashMap<>();
+
             for (String str : strs) {
-                char chars[] = str.toCharArray();
+                char[] chars = str.toCharArray();
                 Arrays.sort(chars);
-                String sortedString = new String(chars);
-                if (map.containsKey(sortedString) == false) {
-                    map.put(sortedString, new ArrayList<>());
+                String sortedStr = new String(chars);
+
+                if (!map.containsKey(sortedStr)) {
+                    map.put(sortedStr, new ArrayList<>());
                 }
-                map.get(sortedString).add(str);
+
+                map.get(sortedStr).add(str);
             }
-            result.addAll(map.values());
-            return result;
+
+            return new ArrayList<>(map.values());
         }
     }
+
 
