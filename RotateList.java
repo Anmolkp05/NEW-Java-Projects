d8125999bdc67eacc0a49ffa623c23bbf3ad8399
Anmol <anmokamalapuri@gmail.com>
2025-06-20 01:42:57 +0530

    first commit

--- RotateList.java
+++ RotateList.java
@@ -8,6 +8,7 @@ public class RotateList {
 
         int k = 2;
         ListNode rotatedList = rotateRight(head, k);
+<<<<<<< HEAD
         RemoveLinkedListElement.printList(rotatedList);
     }
     public static ListNode rotateRight(ListNode head, int k) {
@@ -36,3 +37,51 @@ public class RotateList {
 }
 
 
+=======
+        printList(rotatedList);
+    }
+
+    public static ListNode rotateRight(ListNode head, int k) {
+        if (head == null || head.next == null || k == 0) {
+            return head;
+        }
+
+        int len = 1;
+        ListNode tail = head;
+        while (tail.next != null) {
+            tail = tail.next;
+            len++;
+        }
+
+        k = k % len;
+        if (k == 0) return head;
+
+        tail.next = head;
+
+        for (int i = 0; i < len - k - 1; i++) {
+            head = head.next;
+        }
+
+        ListNode newHead = head.next;
+        head.next = null;
+        return newHead;
+    }
+
+    public static void printList(ListNode head) {
+        while (head != null) {
+            System.out.print(head.val + " ");
+            head = head.next;
+        }
+        System.out.println();
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
+>>>>>>> 99647dd (Clean commit after removing corruption)
