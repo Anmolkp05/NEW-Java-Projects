5f19a9b336bcb62c38991ffa0cac15759e5b7aa2
Anmol <Anmolkamalapuri@gmail.com>
2026-01-07 00:04:18 +0530

    6thjanTwoPointerAndSlidingWindow

--- TwoPointerAndSlidingWindow/CountNumberOfNiceSubarray.java
+++ TwoPointerAndSlidingWindow/CountNumberOfNiceSubarray.java
@@ -12,28 +12,21 @@ public class CountNumberOfNiceSubarray {
         // Initialize counter for total nice subarrays
         int count = 0;
 
-        // Loop over all starting indices
         for (int start = 0; start < nums.length; start++) {
-            // Track number of odd elements in current subarray
             int oddCount = 0;
 
-            // Loop over ending indices starting from 'start'
             for (int end = start; end < nums.length; end++) {
-                // Check if current number is odd
                 if (nums[end] % 2 != 0)
                     oddCount++;
 
-                // If odd count exceeds k, break (not nice)
                 if (oddCount > k)
                     break;
 
-                // If odd count is exactly k, count this subarray
                 if (oddCount == k)
                     count++;
             }
         }
 
-        // Return total nice subarrays
         return count;
     }
 }
