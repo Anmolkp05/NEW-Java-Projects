78f2c889d60fc068ccb12ff4b6e233421c4ef4a1
Anmol <anmokamalapuri@gmail.com>
2026-01-06 14:42:13 +0530

    5thJanTwoPointerAndSliding

--- TwoPointerAndSlidingWindow/BinarySubarrayWithSumK.java
+++ TwoPointerAndSlidingWindow/BinarySubarrayWithSumK.java
@@ -10,7 +10,7 @@ public class BinarySubarrayWithSumK {
     }
     public static int numSubarraysWithSum(int[] nums, int goal) {
 
-
+        /*
         int count = 0;
 
         for (int start = 0; start < nums.length; ++start) {
@@ -26,6 +26,33 @@ public class BinarySubarrayWithSumK {
         }
 
         return count;
+
+         */
+
+            return atMost(nums, goal) - atMost(nums, goal - 1);
+        }
+
+        public static int atMost(int[] nums, int k) {
+            if (k < 0) return 0;
+
+            int left = 0;
+            int sum = 0;
+            int count = 0;
+
+            for (int right = 0; right < nums.length; right++) {  // while(right<n)
+                sum += nums[right];
+
+                while (sum > k) {
+                    sum -= nums[left];
+                    left++;
+                }
+                count += (right - left + 1);
+            }
+
+            return count;
+        }
     }
 
-}
+
+
+
