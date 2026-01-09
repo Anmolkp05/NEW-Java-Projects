b66d30bb158337cf6b78502fea6110472765e917
Anmol <Anmolkamalapuri@gmail.com>
2026-01-10 02:18:06 +0530

    9thjanTwoPointerAndSlidingWindow

--- TwoPointerAndSlidingWindow/MinimumWindowSubsequence.java
+++ TwoPointerAndSlidingWindow/MinimumWindowSubsequence.java
@@ -2,19 +2,56 @@ package TwoPointerAndSlidingWindow;
 
 public class MinimumWindowSubsequence {
     static void main() {
-        String S = "KAYMBAZBDCE";
-        String T = "ABE";
+        String S = "geeksforgeeks";
+        String T = "eksrg";
 
         System.out.println(minWindow(S, T));
     }
     public static String minWindow(String S, String T) {
+
+
+        /*
+
+        //TC = 0(N^2)
+        //SC = 0(1)
+        String answer = "";
+        int minLen = Integer.MAX_VALUE;
+
+        for (int i = 0; i < S.length(); i++) {
+
+            int t = 0;
+
+            for (int j = i; j < S.length(); j++) {
+
+                if (S.charAt(j) == T.charAt(t)) {
+                    t++;
+                }
+
+                if (t == T.length()) {
+
+                    int len = j - i + 1;
+                    if (len < minLen) {
+                        minLen = len;
+                        answer = S.substring(i, j + 1);
+                    }
+                    break;
+                }
+            }
+        }
+        return answer;
+
+         */
+
+
+        //TC = 0(N^2)
+        //SC = 0(1)
 
         String minWin = "";
         int min = S.length() + 1;
 
         int j = 0;
 
-        for (int i = 0; i < S.length(); i++) {
+        for (int i = 0; i < S.length(); i++) {   //0(N)
 
             if (S.charAt(i) == T.charAt(j)) {
                 j++;
@@ -25,7 +62,7 @@ public class MinimumWindowSubsequence {
                 int end = i;
                 j--;
 
-                while (j >= 0) {
+                while (j >= 0) {      //0(N)
                     if (S.charAt(i) == T.charAt(j)) {
                         j--;
                     }
