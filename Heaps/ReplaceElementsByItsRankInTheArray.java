ff31d96c444ab34e6801e574b7bf2cada9c0c2ea
Anmol <Anmolkamalapuri@gmail.com>
2026-03-08 00:01:33 +0530

    7March

--- Heaps/ReplaceElementsByItsRankInTheArray.java
+++ Heaps/ReplaceElementsByItsRankInTheArray.java
@@ -1,34 +1,56 @@
 package Heaps;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class ReplaceElementsByItsRankInTheArray {
 
     public static void main(String[] args) {
         int[] arr = {20, 15, 26, 2, 98, 6};
-        List<Integer> result = replaceWithRank(arr);
-        System.out.println(result);
+        int[] result = replaceWithRank(arr);
+        System.out.println(Arrays.toString(result));
     }
 
-    public static List<Integer> replaceWithRank(int[] arr) {
-        List<Integer> rankList = new ArrayList<>();
+    // Brute Force Approach
+//    public static List<Integer> replaceWithRank(int[] arr) {
+//        List<Integer> rankList = new ArrayList<>();
+//
+//        for (int i = 0; i < arr.length; i++) {
+//            Set<Integer> smaller = new HashSet<>();
+//
+//            for (int j = 0; j < arr.length; j++) {
+//                if (arr[j] < arr[i]) {
+//                    smaller.add(arr[j]);
+//                }
+//            }
+//
+//            int rank = smaller.size() + 1;
+//            rankList.add(rank);
+//        }
+//
+//        return rankList;
+//    }
 
-        for (int i = 0; i < arr.length; i++) {
-            Set<Integer> smaller = new HashSet<>();
+    public static int[] replaceWithRank(int[] arr) {
+        int[] sortedArr = arr.clone();
 
-            for (int j = 0; j < arr.length; j++) {
-                if (arr[j] < arr[i]) {
-                    smaller.add(arr[j]);
-                }
+        Arrays.sort(sortedArr);
+
+        HashMap<Integer, Integer> rankMap = new HashMap<>();
+
+        int rank = 1;
+
+        for (int num : sortedArr) {
+            if (!rankMap.containsKey(num)) {
+                rankMap.put(num, rank);
+                rank++;
             }
-
-            int rank = smaller.size() + 1;
-            rankList.add(rank);
         }
 
-        return rankList;
+        int[] result = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            result[i] = rankMap.get(arr[i]);
+        }
+
+        return result;
     }
 }
