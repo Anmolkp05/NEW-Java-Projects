f5f45e5fbd07f85666d382e8ee4f0eb237ac22ce
Anmol <Anmolkamalapuri@gmail.com>
2026-03-22 23:59:20 +0530

    22March

---
+++ BinaryTree/SymmetricTree.java
@@ -0,0 +1,52 @@
+package BinaryTree;
+
+public class SymmetricTree {
+    public static void main(String[] args) {
+         TreeNode root = new TreeNode(1,
+                new TreeNode(2, new TreeNode(3), new TreeNode(4)),
+                new TreeNode(2, new TreeNode(4), new TreeNode(3))
+        );
+
+        System.out.println(isSymmetric(root)); // true
+    }
+
+    public static boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return isMirror(root.left, root.right);
+    }
+
+    public static boolean isMirror(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
+            return true;
+        }
+        if (left == null || right == null) {
+            return false;
+        }
+        if (left.val != right.val) {
+            return false;
+        }
+        return isMirror(left.left, right.right) &&
+                isMirror(left.right, right.left);
+    }
+}
+
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {}
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
