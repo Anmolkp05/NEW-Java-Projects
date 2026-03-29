de6214b7cae8d051816ca1bb09f397bc8d4f876a
Anmol <Anmolkamalapuri@gmail.com>
2026-03-30 03:53:31 +0530

    29March

--- BitManipulation/AddTwoIntegers.java
+++ BitManipulation/AddTwoIntegers.java
@@ -4,6 +4,11 @@ public class AddTwoIntegers {
     static void main() {
         System.out.println(getSum(2,3));
     }
+
+//    TC = O(1) (constant time)
+//    Sc = 0(1)  Only using:a, b, temp
+
+
     public static int getSum(int a, int b) {
         while (b!=0){
            int temp = (a&b)<<1;
---
+++ BitManipulation/NumberOf1Bits.java
@@ -0,0 +1,51 @@
+package BitManipulation;
+
+public class NumberOf1Bits {
+    static void main() {
+
+        System.out.println(hammingWeight( 2));
+    }
+    public static int hammingWeight(int n) {
+//  Time Complexity: O(1) Loop runs 32 times (fixed) Doesn’t depend on input size
+//  Space Complexity: O(1) Only using a variable count
+//        int count=0 ;
+//        for(int i = 31 ; i>=0 ; i--){
+//        if(((n>>i)&1)==1){
+//               count += 1 ;
+//            }
+//        }
+//        return count;
+
+//  Brian Kernighan’s Algorithm
+//  Time Complexity: O(k) number of set bits (1s)
+//  Space Complexity: O(1) Only using a variable count
+//        int count=0;
+//        while(n!=0){
+//            n=(n&(n-1));
+//            count+=1;
+//
+//
+//        }
+//        return count;
+
+
+
+//        Time Complexity: O(log2(n)) Each division by 2 reduces size
+//  Space Complexity: O(1) Only using a variable count
+//        int count=0;
+//        while(n!=0){
+//            count += (n%2);
+//            n = n/2;
+//        }
+//        return count;
+//
+//    }
+
+
+//  Built-in Method
+//  Time Complexity:O(1) (effectively constant)
+//  Space Complexity: O(1) Only using a variable count
+        return Integer.bitCount(n);
+    }
+
+}
