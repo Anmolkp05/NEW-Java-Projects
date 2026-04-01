5960bf03131d54ef3ef57ae0f06b83fd15937d9e
Anmol <Anmolkamalapuri@gmail.com>
2026-04-02 01:15:23 +0530

    1April

--- Arrays/bestTimeToBuyAndSellStock.java
+++ Arrays/bestTimeToBuyAndSellStock.java
@@ -9,18 +9,17 @@ class bestTimeToBuyAndSellStock{
     }
     public int maxProfit(int[] prices) {
 
-       // Time	O(n²)  (nested loops)
-       // Space	O(1)
-        int profit =0;
-        for(int i=0;i<prices.length;i++){
-            for(int j=i+1;j< prices.length;j++){
-                if((prices[j]-prices[i])>profit){
+        // Time	O(n²)  (nested loops)
+        // Space	O(1)
+        int profit = 0;
+        for (int i = 0; i < prices.length; i++) {
+            for (int j = i + 1; j < prices.length; j++) {
+                if ((prices[j] - prices[i]) > profit) {
                     profit = prices[j] - prices[i];
                 }
-
             }
-
         }
+        return profit;
 
 //        TC = O(n)
 //        SC = 0(1)
@@ -39,15 +38,16 @@ class bestTimeToBuyAndSellStock{
 
         //TC = O(n)
         //SC = 0(1)
-        int min = prices[0];
-        int maxProfit= 0;
-        for(int i =1;i<prices.length;i++){
+//        int min = prices[0];
+//        int maxProfit= 0;
+//        for(int i =1;i<prices.length;i++){
+//
+//            int profit=prices[i]-min;
+//            maxProfit = Math.max(maxProfit,profit);
+//            min=Math.min(min,prices[i]);
+//        }
+//        return maxProfit;
 
-            int profit=prices[i]-min;
-            maxProfit = Math.max(maxProfit,profit);
-            min=Math.min(min,prices[i]);
-        }
-        return maxProfit;
 
     }
 }
