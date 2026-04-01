832b44e3e8a0a4bb8cc065eca4965d0ca3c62a63
Anmol <Anmolkamalapuri@gmail.com>
2026-04-02 01:13:27 +0530

    1April

--- Arrays/bestTimeToBuyAndSellStock.java
+++ Arrays/bestTimeToBuyAndSellStock.java
@@ -9,8 +9,8 @@ class bestTimeToBuyAndSellStock{
     }
     public int maxProfit(int[] prices) {
 
-       // Time	O(n²) ❌ (nested loops)
-       // Space	O(1) ✅
+       // Time	O(n²)  (nested loops)
+       // Space	O(1)
         int profit =0;
         for(int i=0;i<prices.length;i++){
             for(int j=i+1;j< prices.length;j++){
