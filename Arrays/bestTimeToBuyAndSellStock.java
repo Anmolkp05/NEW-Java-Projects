0ce0713cb295df3ddc7cfee5892512cc946638f9
Anmol <Anmolkamalapuri@gmail.com>
2026-04-02 01:12:51 +0530

    1April

--- Arrays/bestTimeToBuyAndSellStock.java
+++ Arrays/bestTimeToBuyAndSellStock.java
@@ -8,17 +8,47 @@ class bestTimeToBuyAndSellStock{
         System.out.println(result);
     }
     public int maxProfit(int[] prices) {
-        int minPrice = Integer.MAX_VALUE;
-        int maxProfit=0;
 
-        for(int i = 0 ; i < prices.length ; i++){
-            if (prices[i] < minPrice) {
-                minPrice= prices[i];
-            }else if(prices[i] - minPrice > maxProfit){
-                maxProfit = prices[i] - minPrice;
+       // Time	O(n²) ❌ (nested loops)
+       // Space	O(1) ✅
+        int profit =0;
+        for(int i=0;i<prices.length;i++){
+            for(int j=i+1;j< prices.length;j++){
+                if((prices[j]-prices[i])>profit){
+                    profit = prices[j] - prices[i];
+                }
+
             }
+
+        }
+
+//        TC = O(n)
+//        SC = 0(1)
+//        int minPrice = Integer.MAX_VALUE;
+//        int maxProfit=0;
+//
+//        for(int i = 0 ; i < prices.length ; i++){
+//            if (prices[i] < minPrice) {  //for buying date
+//                minPrice= prices[i];
+//            }else if(prices[i] - minPrice > maxProfit){  for selling
+//                maxProfit = prices[i] - minPrice;
+//            }
+//        }
+//        return maxProfit;
+
+
+        //TC = O(n)
+        //SC = 0(1)
+        int min = prices[0];
+        int maxProfit= 0;
+        for(int i =1;i<prices.length;i++){
+
+            int profit=prices[i]-min;
+            maxProfit = Math.max(maxProfit,profit);
+            min=Math.min(min,prices[i]);
         }
         return maxProfit;
+
     }
 }
 
