dc4b76663f0fa2478bcc07a0b894a4e406b5359d
Anmol <Anmolkamalapuri@gmail.com>
2026-09-06 21:33:59 +0530

    6/09/26

--- Arrays/MoveZeroes.java
+++ Arrays/MoveZeroes.java
@@ -2,18 +2,22 @@ package Arrays;
 
 class MoveZeroes {
     public void moveZeroes(int[] nums) {
+        //tc = 0(N)
+        //sc=(1)
         int count =0 ;
         for (int i =0;i<nums.length;i++)
         {
             if(nums[i] !=0)
             {
-                nums[count++]=nums[i];
+                nums[count]=nums[i];
+                count++;
             }
 
         }
         while(count<nums.length)
         {
-            nums[count++]=0;
+            nums[count]=0;
+            count++;
         }
     }
 }
