f4cc7948a4be4ac4094c07bbf89354d64d6da831
Anmol <Anmolkamalapuri@gmail.com>
2026-09-07 11:26:32 +0530

    7/09/26

--- Arrays/MissingNumber.java
+++ Arrays/MissingNumber.java
@@ -1,5 +1,6 @@
 package Arrays;
 
+
 public class MissingNumber {
     public static void main(String[] args) {
         int[] nums = {4,6,7,8,9,5,3,0,2,1};
@@ -8,15 +9,47 @@ public class MissingNumber {
     }
 
     public static int missingNumber(int[] nums) {
-        int sum = 0;
-        for (int i = 0; i < nums.length; i++) {
-            sum = sum + nums[i];
+//        //TC = 0(N^2)
+//        //sc = 0(1)
+//        for(int i=0;i<=nums.length;i++){
+//            boolean found = false;
+//            for(int j=0;j<nums.length;j++){
+//                if(nums[j]==i){
+//                    found = true;
+//                    break;
+//                }
+//
+//            }
+//            if(!found){
+//                return i;
+//            }
+//        }
+//        return -1;
+
+
+        int n = nums.length;
+        int[] hash = new int[n + 1];
+        for(int i=0;i<n;i++){
+            hash[nums[i]] =1;
         }
+        for(int j=0;j<hash.length;j++){
+            if(hash[j]==0){
+                return j;
+            }
+        }
+        return -1;
 
-            int actualSum = (nums.length * (nums.length + 1) / 2);
-            int missingNum = actualSum - sum;
 
-            return missingNum;
+
+//        int sum = 0;
+//        for (int i = 0; i < nums.length; i++) {
+//            sum = sum + nums[i];
+//        }
+//
+//            int actualSum = (nums.length * (nums.length + 1) / 2);
+//            int missingNum = actualSum - sum;
+//
+//            return missingNum;
         }
     }
 
