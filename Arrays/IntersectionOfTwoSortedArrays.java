0c94c7b7e4c2d38ee71c03248b31d95fdd875c7c
Anmol <Anmolkamalapuri@gmail.com>
2026-09-08 20:45:23 +0530

    8/09/26

--- Arrays/IntersectionOfTwoSortedArrays.java
+++ Arrays/IntersectionOfTwoSortedArrays.java
@@ -11,25 +11,51 @@ public class IntersectionOfTwoSortedArrays {
     public static ArrayList<Integer> intersection(int arr1[], int arr2[]) {
         //TC = 0(a*b)
         //sc = 0(b)
+//        ArrayList<Integer> ans = new ArrayList<>();
+//        int a = arr1.length;
+//        int b = arr2.length;
+//        int[] visited = new int[b];
+//        for(int i =0;i<a;i++){
+//            for(int j=0;j<b;j++){
+//                if(arr1[i]==arr2[j] && visited[j]==0){
+//                    ans.add(arr1[i]);
+//                    visited[j]++ ;
+//                    break;
+//
+//                }
+//                if(arr1[i]<arr2[j]){
+//                    break;
+//                }
+//            }
+//        }
+//
+//
+//        return ans;
+
+        //TC = 0(A+B)
+        //SC = 0(1)
         ArrayList<Integer> ans = new ArrayList<>();
         int a = arr1.length;
         int b = arr2.length;
-        int[] visited = new int[b];
-        for(int i =0;i<a;i++){
-            for(int j=0;j<b;j++){
-                if(arr1[i]==arr2[j] && visited[j]==0){
-                    ans.add(arr1[i]);
-                    visited[j]++ ;
-                    break;
 
-                }
-                if(arr1[i]<arr2[j]){
-                    break;
-                }
+        int i=0;
+        int j = 0;
+        while(i<a && j<b){
+            if(arr1[i]<arr2[j]){
+                i++;
             }
+            else if(arr1[i]>arr2[j]){
+                j++;
+            }
+            else{
+                ans.add(arr1[i]);
+                i++;
+                j++;
+            }
+
+
         }
-
-
         return ans;
+
     }
 }
